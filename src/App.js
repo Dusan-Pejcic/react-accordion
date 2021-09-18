4b00713c9a571cacdbf84d979bafccd00650417e
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-18 06:32:23 +0200

    Questions list made, question component, and stale added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className="App">
-      <h1>Accordion project</h1>
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          { questions.map((question) => {
+            return(
+              <SingleQuestion key={question.id} {...question} />
+            );
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
